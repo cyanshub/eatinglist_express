ab24e3907d5be29ced5ddd2b7ac345aeb0f5d9d0
travel15740 <travel15740@gmail.com>
2023-06-02 11:44:25 +0800

    將show頁面動態化: 使用find陣列方法, 篩選對應點擊的餐廳

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ app.get('/', (rep,res)=>{
 // 參考 a 元素的 href 設定路由(應用動態路由的概念)
 app.get('/restaurants/:restaurant_id', (req,res)=>{
   console.log('restaurant_id: ',req.params.restaurant_id)
+  const restaurant_id = Number(req.params.restaurant_id)
 
-  // 先行測試檔案
-  const restaurant = restaurants.results[0]
+  // 使用 find 陣列方法, 依動態路由 id 找出對應點擊的餐廳
+  const restaurant = restaurants.results.find(restaurant => restaurant.id === restaurant_id  )
 
   // 將外部JSON檔案: 餐廳清單傳入局部頁面引擎
   res.render('show', {restaurant:restaurant})
